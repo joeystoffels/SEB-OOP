9d04e72eadb5b5a5c7a117c22b99d0a06ab09a8c
Joey Stoffels <joey.stoffels@exe.energy>
2018-04-29 11:21:38 +0200

    make that a SpriteObject instead of GameObject

--- src/main/java/nl/han/ica/airspaceinvaders/objects/enemies/Air.java
+++ src/main/java/nl/han/ica/airspaceinvaders/objects/enemies/Air.java
@@ -1,15 +1,21 @@
 package nl.han.ica.airspaceinvaders.objects.enemies;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
 import nl.han.ica.airspaceinvaders.interfaces.IEnemy;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
 import processing.core.PGraphics;
 
 import java.util.List;
 
-public class Air extends GameObject implements IEnemy {
+public class Air extends SpriteObject implements IEnemy {
 
     private int health;
     private int shield;
+
+    public Air(Sprite sprite) {
+        super(sprite);
+    }
 
     /**
      * @param collidedGameObjects
@@ -21,11 +27,6 @@ public class Air extends GameObject implements IEnemy {
 
     @Override
     public void update() {
-
-    }
-
-    @Override
-    public void draw(PGraphics g) {
 
     }
 
--- src/main/java/nl/han/ica/airspaceinvaders/objects/enemies/Ground.java
+++ src/main/java/nl/han/ica/airspaceinvaders/objects/enemies/Ground.java
@@ -1,15 +1,21 @@
 package nl.han.ica.airspaceinvaders.objects.enemies;
 
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.SpriteObject;
 import nl.han.ica.airspaceinvaders.interfaces.IEnemy;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
 import processing.core.PGraphics;
 
 import java.util.List;
 
-public class Ground extends GameObject implements IEnemy {
+public class Ground extends SpriteObject implements IEnemy {
 
     private int health;
     private int shield;
+
+    public Ground(Sprite sprite) {
+        super(sprite);
+    }
 
     /**
      * @param collidedGameObjects
@@ -21,11 +27,6 @@ public class Ground extends GameObject implements IEnemy {
 
     @Override
     public void update() {
-        
-    }
-
-    @Override
-    public void draw(PGraphics g) {
 
     }
 
