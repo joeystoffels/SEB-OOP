36fb3615aba6de4c80bf011146e729c345ff103f
Joey Stoffels <joey.stoffels@exe.energy>
2018-05-11 00:03:11 +0200

    PowerUp sprite added

--- src/main/java/nl/han/ica/airspaceinvaders/gameobjects/enemies/Air.java
+++ src/main/java/nl/han/ica/airspaceinvaders/gameobjects/enemies/Air.java
@@ -99,7 +99,7 @@ public class Air extends SpriteObject implements IFlyingObject {
 
             this.airspaceInvadersGame.addGameObject(
                     new PowerUp(this.airspaceInvadersGame,
-                    AssetLoader.getSprite("enemy/" + PowerUpTypes.values()[randomNr].toString() + ".png", 20),
+                    AssetLoader.getSprite("powerup/PowerUp.png", 10),
                     PowerUpTypes.values()[randomNr].toString()), this.getCenterXPos() - (this.getWidth() / 2), this.getCenterYPos() - (this.getHeight() / 2)
             );
         }
